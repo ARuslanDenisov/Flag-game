af5e4e6195b3faf9cfd936e198c01734b5abe1c9
Denisov Ruslan <denisovitapi@gmail.com>
2024-05-26 14:07:06 +0700

    Initial Commit

---
+++ Hudson 1/Hudson_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hudson_1App.swift
+//  Hudson 1
+//
+//  Created by Руслан on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Hudson_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
